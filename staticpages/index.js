6465b85e31bedd383162a2957c427d770b4808ff
Sinduff <10016151@gmit.ie>
2020-12-27 17:16:36 +0000

    work on getting update function working + modifications to index.html code to allow for a global variable to store variables to be called by functions

--- staticpages/index.js
+++ staticpages/index.js
@@ -1,49 +1,176 @@
-function populateTable(){
-    // function to populate the table from the DB to getAll()
-    //ajax getAll
-    //
+let formValues = [];
+
+function showCreate() {
+	//find the div for 'display' @ line 37 and show
+	document.getElementById('display').style.display = "none";
+	document.getElementById('update-button').style.display = "block";
+	document.getElementById('update-button').style.display = "none";
+	document.getElementById('create-update').style.display = "block";
+}
+
+function showUpdate(thisElem) {
+	//function to read the data in the row to be updated
+
+
+	populateForm(formValues[thisElem]);
+
+	//hide or show the buttons
+	document.getElementById('display').style.display = "none";
+	document.getElementById('update-button').style.display = "block";
+	document.getElementById('create-button').style.display = "none";
+	document.getElementById('create-update').style.display = "block";
+}
+
+function populateForm(eeUpdate) {
+	//same code as function getEEFromForm
+	var form = document.getElementById('createUpdateForm');
+
+	form.querySelector('input[name="eeID"]').value = eeUpdate.eeID;
+	form.querySelector('input[name="eeID"]').disabled = true;
+
+	form.querySelector('input[name="firstName"]').value = eeUpdate.firstName;
+	form.querySelector('input[name="lastName"]').value = eeUpdate.lastName;
+	form.querySelector('input[name="address"]').value = eeUpdate.address;
+}
+
+function doCreate() {
+	//find the div for 'display' @ line 37 and show
+	//console.log("in doCreate")
+	let EE = getEEFromForm();
+	//code to put the data to the server
+	$.ajax({
+		url: "http://127.0.0.1:5000/employees",
+		method: 'POST',
+		//convert data to a string to allow it to be passed to the DB
+		data: JSON.stringify(EE),
+		dataType: 'JSON',
+		contentType: "application/json; charset=utf-8",
+		success: function (result) {
+			console.log(result);
+			//add book to table if the update has worked
+			//addEEtoTable(EE)
+			showDisplay();
+			populateTable();
+		},
+		error: function (xhr, status, error) {
+			console.log("error " + error);
+		}
+	});
+	//showDisplay()
+}
+
+function showDisplay() {
+	//find the div for 'display' @ line 37 and show
+	document.getElementById('display').style.display = "block";
+	document.getElementById('create-update').style.display = "none";
+}
+
+function doUpdate() {
+	let EE = getEEFromForm();
+	//update server
+	console.log(EE, "doUpdate");
+	updateServer(EE);
+}
+
+function updateServer(employee) {
+	//ajax call to the server
+	$.ajax({
+		url: "/employees/" + employee.empID, //EE.eeID,
+		method: "PUT",
+		//convert data to a string to allow it to be passed to the DB
+		data: JSON.stringify(employee),
+		dataType: 'JSON',
+		contentType: "application/json; charset=utf-8",
+		success: function (result) {
+			console.log(result);
+			populateTable();
+			showDisplay();
+		},
+		error: function (xhr, status, error) {
+			console.log("error " + error);
+		}
+	});
+}
+
+function updateTableRow(EE) {
+	console.log("updating table");
+}
+
+function doDelete(thisElem) {
+	let id = formValues[thisElem].eeID;
+	console.log(id);
+	$.ajax({
+		url: "/employees/" + id,
+		method: "DELETE",
+		data: "",
+		dataType: "JSON",
+		contentType: "application/json; charset=utf-8",
+		success: function (result) {
+			populateTable();
+		},
+		error: function (xhr, status, error) {
+			//console.log()
+			console.log("error " + error);
+		}
+	});
+}
+
+function getEEFromForm() {
+	var form = document.getElementById('createUpdateForm');
+	var EE = {};
+	//commented out this as the primary key is auto incremented and code won't update unless it's blank
+	//undefined is returned to html page, but on refresh ..primary key is visible
+	EE.empID = form.querySelector('input[name="eeID"]').value;
+	EE.firstName = form.querySelector('input[name="firstName"]').value;
+	EE.lastName = form.querySelector('input[name="lastName"]').value;
+	EE.address = form.querySelector('input[name="address"]').value;
+	//sanity check for doCreate() function
+	console.log(EE);
+	return EE;
+}
+
+function populateTable() {
+    //code to clear the table before starting to load in the data
+	const tableElem = document.getElementById("employeeTable");
+	tableElem.innerHTML = '';
     
-    //employee = {
-        //empID:"5",
-        //firstName:"Jane",
-        //lastName:"Doe",
-        //address:"NY, NY"                   
-    //}
     employee = '';
-     $.ajax({
-        url:"http://127.0.0.1:5000/employees",
-        method:'GET',
-        data:'',
-        dataType:'JSON',
-        contentType: "application/json; charset=utf-8",
-        success:function(result){
-             //for (const item in result)
-             result.forEach(addEEtoTable);
-             
-        },
-        error:function(xhr,status,error){
-            console.log("error" +error + "code"+status)
-        }
-    })
-    //addEEtoTable(employee)
- }
-function addEEtoTable(row){
-    //console.log("working so far")
-    const tableElem = document.getElementById("employeeTable")
-    const rowElem = tableElem.insertRow(-1)
-    rowElem.setAttribute("id",row.eeID)
-    //rowElem.setAttribute("id",employee.firstName)
-    const cell1 = rowElem.insertCell(0);
-    cell1.innerHTML = row.eeID;
-    const cell2 = rowElem.insertCell(1);
-    cell2.innerHTML = row.firstName;
-    const cell3 = rowElem.insertCell(2);
-    cell3.innerHTML = row.lastName;
-    const cell4 = rowElem.insertCell(3);
-    cell4.innerHTML = row.address;
-    const cell5 = rowElem.insertCell(4);
-    cell5.innerHTML = '<button onclick="showUpdate(this)">Update</button>';
-    const cell6 = rowElem.insertCell(5);
-    cell6.innerHTML = '<button onclick="doDelete(this)">Delete</button>';
-    }
- populateTable()
+	$.ajax({
+		url: "http://127.0.0.1:5000/employees",
+		method: 'GET',
+		data: '',
+		dataType: 'JSON',
+		contentType: "application/json; charset=utf-8",
+		success: function (result) {
+			formValues = result;
+			formValues.forEach((fe, indx) => addEEtoTable(fe, indx));
+		},
+		error: function (xhr, status, error) {
+			console.log("error" + error + "code" + status);
+		}
+	});
+}
+
+function addEEtoTable(EE, indx) {
+    const tableElem = document.getElementById("employeeTable");
+    
+    //add a new row to the table
+	const rowElem = tableElem.insertRow(-1);
+	rowElem.setAttribute("id", EE.eeID);
+
+	const cell1 = rowElem.insertCell(0);
+	cell1.innerHTML = EE.eeID;
+	const cell2 = rowElem.insertCell(1);
+	cell2.innerHTML = EE.firstName;
+	const cell3 = rowElem.insertCell(2);
+	cell3.innerHTML = EE.lastName;
+	const cell4 = rowElem.insertCell(3);
+	cell4.innerHTML = EE.address;
+	const cell5 = rowElem.insertCell(4);
+	cell5.innerHTML = `<button onclick="showUpdate(${indx})">Update</button>`;
+	const cell6 = rowElem.insertCell(5);
+	cell6.innerHTML = `<button onclick="doDelete(${indx})">Update</button>`;
+}
+populateTable();
+
+
